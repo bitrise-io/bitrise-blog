40bddc9a9a52d3c0575e2bac27eeaabeae3bb733
Gergely Bekesi <gergely.bekesi@bitrise.io>
2017-08-24 15:11:14 +0200

    Implementation of category index and post show additions

--- app/controllers/buttercms/categories_controller.rb
+++ app/controllers/buttercms/categories_controller.rb
@@ -1,5 +1,11 @@
 class Buttercms::CategoriesController < Buttercms::BaseController
+  before_action :set_variables
+
   def show
     @category = ButterCMS::Category.find(params[:slug], :include => :recent_posts)
+  end
+private
+  def set_variables
+    @categories = ButterCMS::Category.all
   end
 end
