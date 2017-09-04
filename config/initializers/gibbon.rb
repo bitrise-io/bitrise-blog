a3ea0d92d6fd35eea691b615c04c023da0e2b14b
Gergely Bekesi <gergely.bekesi@bitrise.io>
2017-09-04 12:12:36 +0200

    Remove Gibbon initializer

--- config/initializers/gibbon.rb
+++
@@ -1,6 +0,0 @@
-Gibbon::Request.api_endpoint = ENV['MAILCHIMP_API_SERVER']
-Gibbon::Request.api_key = ENV['MAILCHIMP_API_TOKEN']
-Gibbon::Request.timeout = 15
-Gibbon::Request.open_timeout = 15
-Gibbon::Request.symbolize_keys = true
-Gibbon::Request.debug = false
