0d5a0b582f1b4c0fccb68f34be66172b9b2e2530
Gergely Bekesi <gergely.bekesi@bitrise.io>
2017-09-04 13:25:41 +0200

    Clean debug code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,5 @@
 module ApplicationHelper
 	def svg(filename)
-		puts ">>>>>>>>>"
-		puts filename
-		puts filename + '.svg'
-		puts Rails.application.assets_manifest.find_sources(filename + '.svg').inspect
-		Rails.application.assets_manifest.find_sources(filename + '.svg').each do |item|
-			puts item.inspect
-		end
-		puts ">>>>>>>>>"
-
 		svg_file_content = Rails.application.assets_manifest.find_sources(filename + '.svg').first
 		return raw(svg_file_content) if svg_file_content
 		return "(svg not found)"
