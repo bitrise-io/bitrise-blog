7470198a25449f32dc550e0a8ca61206753cb223
Gergely Bekesi <gergely.bekesi@bitrise.io>
2017-09-04 08:35:19 +0200

    Subscription backend reimplemented without Gibbon

--- app/controllers/buttercms/base_controller.rb
+++ app/controllers/buttercms/base_controller.rb
@@ -9,26 +9,39 @@ class Buttercms::BaseController < ActionController::Base
 		respond_to do |format|
 			if !subscribe_params[:email].eql?("")
 				begin
-					Gibbon::Request.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(
-						body: {
+					encoded_authorization_token = "Basic " + Base64.strict_encode64("slapec:#{ENV['MAILCHIMP_API_TOKEN']}")
+
+					query = "query=" + subscribe_params[:email]
+					response = RestClient::Request.execute(
+						method: :get,
+						url: "#{ENV['MAILCHIMP_API_SERVER']}/search-members?#{query}",
+						headers: {
+							"Authorization": encoded_authorization_token
+						}
+					)
+					response_body = JSON.parse(response.body)
+					if response_body["exact_matches"]["total_items"] > 0
+						format.json{ render json: {message: "already subscribed"}.to_json(), status: :ok }
+					end
+
+					response = RestClient::Request.execute(
+						method: :post,
+						url: "#{ENV['MAILCHIMP_API_SERVER']}/lists/#{ENV['MAILCHIMP_LIST_ID']}/members",
+						headers: {
+							"Authorization": encoded_authorization_token,
+							"Content-Type": "application/json"
+						},
+						payload: {
 							email_address: subscribe_params[:email],
 							status: "subscribed"
-						})
-
-					format.json{ render json: {message: "ok"}, status: :ok }
-				rescue Gibbon::MailChimpError => ex
-					status = ex.body[:status]
-					status_text = ex.body[:title]
-					case status
-					when 400
-						format.json{ render json: {message: "existing email"}, status: :bad_request } if status_text.eql?("Member Exists")
-						format.json{ render json: {message: "invalid email"}, status: :bad_request } if status_text.eql?("Invalid Resource")
-					else
-						format.json{ render json: {message: "unknown error"}, status: :bad_request }
-					end
+						}.to_json()
+					)
+					format.json{ render json: response.to_json(), status: :ok }
+				rescue RestClient::ExceptionWithResponse
+					format.json{ render json: {message: "invalid email"}.to_json(), status: :internal_server }
 				end
 			else
-				format.json{ render json: {message: "empty email"}, status: :bad_request }
+				format.json{ render json: {message: "empty email"}.to_json(), status: :bad_request }
 			end
 		end
 	end
