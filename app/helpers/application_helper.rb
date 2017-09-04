597ac68df9012d507f8ea57ab7b7e6a290678ddd
Gergely Bekesi <gergely.bekesi@bitrise.io>
2017-09-04 12:20:04 +0200

    Deploy WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 	def svg(filename)
+		puts filename
 		svg_file_content = Rails.application.assets_manifest.find_sources(filename + '.svg').first
 
 		return raw(svg_file_content) if svg_file_content
