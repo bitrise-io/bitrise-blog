10e8c464572288f053c0b4f92f5525cca4e5d7e8
Gergely Bekesi <gergely.bekesi@bitrise.io>
2017-11-21 08:41:53 +0100

    Sipmlify solution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,17 +4,4 @@ module ApplicationHelper
 		return raw(svg_file_content) if svg_file_content
 		return "(svg not found)"
 	end
-
-	def html_escape_characters(text)
-		expressions_to_replace = [
-			["&quot;", "\""],
-			["&#39;", "'"]
-		]
-
-		expressions_to_replace.each do |exp|
-			text = text.gsub(exp.first, exp.last)
-		end
-
-		return text
-	end
 end
