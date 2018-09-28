109b1ac4cb62cca5af23b285d0399a21ddc9edea
Norbert Kovach <doomsayer13@users.noreply.github.com>
2018-09-28 16:33:31 +0300

    Update sitemap handler (#39)

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,5 +1,5 @@
 class SitemapsController < ApplicationController
   def sitemap
-    redirect_to "https://s3.amazonaws.com/#{ ENV['AWS_S3_BUCKET'] }/sitemaps/sitemap.xml.gz"
+    redirect_to "https://s3.amazonaws.com/#{ ENV['AWS_S3_BUCKET'] }/sitemap.xml"
   end
 end
