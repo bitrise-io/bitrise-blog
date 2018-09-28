3ad96e74aafad9a54af8334c91ab5dc344c609c5
doomsayer13 <doomsayer13@gmail.com>
2018-09-28 16:04:15 +0200

    Fix sitemap handling

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,5 +1,5 @@
 class SitemapsController < ApplicationController
   def sitemap
-    redirect_to "https://s3.amazonaws.com/#{ ENV['AWS_S3_BUCKET'] }/sitemap.xml"
+    redirect_to "https://s3.amazonaws.com/#{ ENV['AWS_S3_BUCKET'] }/sitemaps/sitemap.xml"
   end
 end
